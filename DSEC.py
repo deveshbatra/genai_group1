e6cdb86c2458ec31baf62a40ad275724c5035ea8
conorhamill36 <conor.hamill@ed.ac.uk>
2023-11-08 17:56:31 +0000

    added in function for feedback

--- DSEC.py
+++ DSEC.py
@@ -1,6 +1,6 @@
 import numpy as np
 import pandas as pd
-
+from pprint import pprint
 import openai
 from pptx import Presentation
 
@@ -37,7 +37,7 @@ def reword_text_with_gpt4(text, audience_type, shape_type):
             model="gpt-4-0613",  # Replace with the appropriate model
             messages=[
                 {
-                    "role": "system", 
+                    "role": "system",
                     "content": (
                         "You are an assistant that rewords sentences to be clear and concise. Your output will be no longer than the input in length." +
                         " I am presenting to " + audience_type + ", so make it suitable for this audience." + title_prompt
@@ -60,7 +60,7 @@ def create_exec_summary(text, audience_type):
             model="gpt-4-0613",  # Replace with the appropriate model
             messages=[
                 {
-                    "role": "system", 
+                    "role": "system",
                     "content": (
                         "You are an assistant that creates executive summaries." +
                         " I am presenting to " + audience_type + ", so make it suitable for this audience. Produce a simple five bullet point summary"
@@ -77,13 +77,13 @@ def create_exec_summary(text, audience_type):
         return text  # Return the original text if an error occurs
 # Function to process the PowerPoint file
 def process_presentation(
-    input_file_path, 
-    output_file_path, 
+    input_file_path,
+    output_file_path,
     audience_type,
     executive_summary_slide = False):
     # Load the presentation
     prs = Presentation(input_file_path)
-    
+
     # Iterate through each slide and each text box
     all_text = ""
     for slide_number, slide in enumerate(prs.slides):
@@ -97,7 +97,7 @@ def process_presentation(
                 shape.text = reworded_text
                 all_text = all_text + ". " + original_text
         print(f"Processed slide {slide_number + 1}")
-    
+
     if executive_summary_slide == True:
         slide_layout = prs.slide_layouts[1]
         slide_exec = prs.slides.add_slide(slide_layout)
@@ -107,8 +107,55 @@ def process_presentation(
     prs.save(output_file_path)
     print(f"Presentation saved to {output_file_path}")
 
+FEEDBACK_PROMPT = """
+Pretend you are a assistant focussing on making presentations accessible for people with different conditions.
+The image is a slide from a presentation. Please return bullet points on the accessibility of the slide. For each bullet point, include 1. what aspect of the slide isn't accessible, 2. why this isn't accessible for a certain person, 3. how this could be improved.
+Please consider:
+- Colour-blind people
+- People with other visual issues
+- The choice of fonts
+- The choice of colours
+- The style of graphics
+
+Respond with maximum three bullet points Each bullet point should be relevant to the slide.
+"""
+
+with open("api_key.txt") as f:
+    API_KEY = f.read()
+
+# Function to encode the image
+def encode_image(image_path):
+    with open(image_path, "rb") as image_file:
+        return base64.b64encode(image_file.read()).decode('utf-8')
 
 
+def image_feedback(image_filepath):
+    # not sure if API_KEY needs to be preovided both times
+    openai.api_key = API_KEY
+    client = OpenAI(api_key=API_KEY)
+
+    # Getting the base64 string
+    base64_image = encode_image(image_filepath)
+
+    response = client.chat.completions.create(
+        model="gpt-4-vision-preview",
+        messages=[
+            {
+                "role": "user",
+                "content": [
+                    {"type": "text", "text": FEEDBACK_PROMPT},
+                    {
+                        "type": "image_url",
+                        "image_url": {
+                          "url": f"data:image/jpeg;base64,{base64_image}"
+                    },
+                }
+                    ],
+                }
+            ],
+            max_tokens=300,
+            )
+    pprint(response.choices[0].message.content)
 
 # Example usage
 input_file_path = "C:/Users/Administrator/Documents/GPTB4.pptx"
