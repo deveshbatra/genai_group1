5e1cb1dabd509719fea1ba3234b79ee48734c3fd
Akmal Rafiq <42502553+akyrafiq@users.noreply.github.com>
2023-11-08 22:16:52 +0000

    Evneing work
    Changes made:
    - New function created to enable you to turn .docx ad .pdf files into summary powerpoints
    --Tested it on my brothers gcse homework, a itinery for a Singapore holiday and conor's ABM Paper
    -- A blank powerpoint has bene left in to use as the template theme for these
    -I have wrapped the existing process_presentation function into a document_to_ppt function- just set your file type in the function.
    -folders for inputs and outputs have been created for tidiness

--- DSEC.py
+++ DSEC.py
@@ -2,13 +2,16 @@ import numpy as np
 import pandas as pd
 import openai
 from pptx import Presentation
+from pptx.util import Cm, Pt
 from docx import Document
+from pypdf import PdfReader
+import json
 
 # Replace 'your_api_key_here' with your actual OpenAI API key
-openai.api_key = ''
+openai.api_key = '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'
 
 from openai import OpenAI
-client = OpenAI(api_key = '')
+client = OpenAI(api_key = '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')
 # Function to reword text using GPT-4
 #def reword_text_with_gpt4(text):
 #    try:
@@ -26,7 +29,10 @@ client = OpenAI(api_key = '')
 #        print(f"An error occurred: {e}")
 #        return text  # Return the original text if an error occurs
 
-def reword_text_with_gpt4(text, audience_type, shape_type):
+def reword_text_with_gpt4(
+    text: str, 
+    audience_type: str, 
+    shape_type: str) -> str:
     if len(text) == 0:
         return text
     try:
@@ -55,7 +61,9 @@ def reword_text_with_gpt4(text, audience_type, shape_type):
         print(f"An error occurred: {e}")
         return text  # Return the original text if an error occurs
 
-def create_exec_summary(text, audience_type):
+def create_exec_summary(
+    text: str, 
+    audience_type: str) -> str:
     try:
         # Use the OpenAI API to get a response
         response = openai.chat.completions.create(
@@ -77,7 +85,10 @@ def create_exec_summary(text, audience_type):
     except Exception as e:  # Catch a general exception
         print(f"An error occurred: {e}")
         return text  # Return the original text if an error occurs
-def create_ppt_feedback(text, audience_type):
+
+def create_ppt_feedback(
+    text: str, 
+    audience_type: str) -> str:
     try:
         # Use the OpenAI API to get a response
         response = openai.chat.completions.create(
@@ -103,9 +114,9 @@ def create_ppt_feedback(text, audience_type):
 
 # Function to process the PowerPoint file
 def process_presentation(
-    input_file_path, 
-    output_file_path, 
-    audience_type,
+    input_file_path: str, 
+    output_file_path: str, 
+    audience_type: str,
     executive_summary_slide = False):
     # Load the presentation
     prs = Presentation(input_file_path)
@@ -131,6 +142,13 @@ def process_presentation(
         slide_exec.placeholders[0].text = "Executive Summary"
         slide_exec.placeholders[1].text = create_exec_summary(all_text, audience_type)
 
+        tf = slide_exec.placeholders[1].text_frame
+            
+
+        tf.word_wrap = True
+        #for paragraph in tf.paragraphs:
+        #    paragraph.font.size = Pt(26)
+
         feedback = create_ppt_feedback(all_text, audience_type)
         f = open("feedback.txt", "a")
         f.write(feedback)
@@ -141,74 +159,167 @@ def process_presentation(
 
 
 
-def getText(filename):
-    doc = docx.Document(filename)
-    fullText = []
-    for para in doc.paragraphs:
-        fullText.append(para.text)
-    return '\n'.join(fullText)
+def getText(
+    filename: str,
+    file_format: str) -> str:
+    if file_format == "docx":
+        doc = Document(filename)
+        fullText = []
+        for para in doc.paragraphs:
+            fullText.append(para.text)
+        return '\n'.join(fullText)
+    elif file_format =="pdf":
+        reader = PdfReader(filename)
+        text = ""
+        for page in reader.pages:
+            text += page.extract_text() + "\n"
+        if len(text) > 5000:
+            print("PDF file is too large so only taking the first 5,000 characters")
+            text = text[:5000]
+        return text
 
-def document_to_ppt(doc,ppt, audience_type, slide_number):
+def document_to_ppt(
+    doc: str,
+    ppt: str, 
+    audience_type: str, 
+    slide_number: float, 
+    file_format = "docx"):
 
-    
-    soup = bs(open(doc).read())
-    [s.extract() for s in soup(['style', 'script'])]
-    tmpText = soup.get_text()
-    text = "".join("".join(tmpText.split('\t')).split('\n')).strip()
-    print(text)
+    if file_format == "pptx":
+        print("Rephrasing your powerpoint for "+ audience_type)
+        process_presentation(
+            input_file_path = doc, 
+            output_file_path = ppt, 
+            audience_type = audience_type,
+            executive_summary_slide = True)
+    else:
+        text = getText(doc, file_format)
 
-    try:
-        # Use the OpenAI API to get a response
-        response = openai.chat.completions.create(
-            model="gpt-4-0613",  # Replace with the appropriate model
-            messages=[
-                {
-                    "role": "system", 
-                    "content": (
-                        "You are an assistant that provides powerpoints form word document inputs." +
-                        " I am presenting to " + audience_type + ", so convert this document into a powerpoint suitable for this audience." +
-                       "Ensure it has " + str(slide_number + 1) + " slides. The first slide is a title slide and the other slides are in the form of title and contents." +
-                       "Provide  the answer as a dictionary for python of the form {1:{'title':'title','subtitle: 'appropriate subtitle'},2:{'title':'title','contents: 'bullet pointed contents'},3:{'title':'title','contents: 'bullet pointed contents'}... etc}"
+        try:
+            # Use the OpenAI API to get a response
+            response = openai.chat.completions.create(
+                model="gpt-4-0613",  # Replace with the appropriate model
+                messages=[
+                    {
+                        "role": "system", 
+                        "content": (
+                            "You are an assistant that provides powerpoints form word document inputs." +
+                            " I am presenting to " + audience_type + ", so convert this document into a powerpoint suitable for this audience." +
+                           "Ensure it has " + str(slide_number + 1) + " slides. The first slide is a title slide and the other slides are in the form of title and contents." +
+                           "Provide  the answer as a dictionary for python of the form {'Slide 1':{'title':'title','subtitle: 'appropriate subtitle'},'Slide 2':{'title':'title','contents: unnumbered list of bullet points},'Slide 3':{'title':'title','contents: unnumbered list of bullet points}... etc}."+
+                           " This string will be converted into a dictionary so ensure the bullet points are able to be read into a json as a list. The list length can't be longer than five. only print out the dictionary."
 
-                                )
-                    },
-                {"role": "user", "content": text}
-            ]
-        )
-        # Assuming the last message in the list will be the assistant's response
-        ppt_dict =  str(response.choices[0].message.content)#.text.strip()
-        print(ppt_dict)
-        prs = Presentation()
+                                    )
+                        },
+                    {"role": "user", "content": text}
+                ]
+            )
+            # Assuming the last message in the list will be the assistant's response
+            ppt_dict =  str(response.choices[0].message.content)
 
-        title_layout = prs.slide_layouts[0]
-        normal_layout = prs.slide_layouts[1]
-        title_slide = prs.slides.add_slide(title_layout)
-        title_slide.placeholders[0].text = ppt_dict[1]["title"]
-        title_slide.placeholders[1].text = ppt_dict[1]["subtitle"]
+            #print(ppt_dict)
 
-        for i in range(2,slide_numer +1):
-            slide = prs.slides.add_slide(title_layout)
-            slide.placeholders[0].text = ppt_dict[i]["title"]
-            slide.placeholders[1].text = ppt_dict[i]["contents"]
+            json_acceptable_string = ppt_dict.replace("'", "\"")
 
-        # Save the presentation
-        prs.save(ppt)
-        print(f"Presentation saved to {ppt}")
+            #print(json_acceptable_string)
+            try:
+                ppt_dict = json.loads(json_acceptable_string, strict = False)
 
-    except Exception as e:  # Catch a general exception
-        print(f"An error occurred: {e}")
+        
+                prs = Presentation("theme.pptx")
+
+                print("Making title slide")
+                title_layout = prs.slide_layouts[0]
+                normal_layout = prs.slide_layouts[1]
+                title_slide = prs.slides.add_slide(title_layout)
+                title_slide.placeholders[0].text = ppt_dict["Slide 1"]["title"]
+                title_slide.placeholders[1].text = ppt_dict["Slide 1"]["subtitle"]
+
+                for i in range(2,slide_number +2):
+                    print("Making Slide Number " + str(i))
+                    slide = prs.slides.add_slide(normal_layout)
+                    slide.placeholders[0].text = ppt_dict["Slide "+str(i)]["title"]
+                    tf = slide.placeholders[1].text_frame
+            
+                    for j in ppt_dict["Slide "+str(i)]["contents"]:
+                 
+                        p = tf.add_paragraph()
+                        p.text = j
+                        p.level = 0
+
+                    tf.word_wrap = True
+                    for paragraph in tf.paragraphs:
+                        paragraph.font.size = Pt(26)
+                # Save the presentation
+                prs.save(ppt)
+                print(f"Presentation saved to {ppt}")
+
+            except Exception as e:  # Catch a general exception
+                print(f"An error occurred with OpenAI Output- try rerunning the query")
+
+        except Exception as e:  # Catch a general exception
+            print(f"An error occurred: {e}")
 
     
 # Example usage
 wd = "C://Users//Administrator//Documents//GitHub//genai_group1"
 import os
 os.chdir(wd)
-input_file_path =  "GPTB4.pptx"
-output_file_path = "GPTA4"
-output_file_path_technical = "GPTA4_tech.pptx"
-output_file_path_baby = "GPTA4_babies.pptx"
+input_file_path =  "inputs//GPTB4.pptx"
+output_file_path = "outputs//GPTA4"
+output_file_path_technical = "outputs//GPTA4_tech.pptx"
+output_file_path_baby = "outputs//GPTA4_babies.pptx"
 audience_type1 = "a technical audience"
 audience_type2 = "a bunch of five year olds who like thomas the tank engine"
-document_to_ppt("Singapore Day 1.docx","singapore.pptx",audience_type2, 3)
-#process_presentation(input_file_path, output_file_path_technical, audience_type1,executive_summary_slide = True)
-#process_presentation(input_file_path, output_file_path_baby, audience_type2)
+audience_type3 = "people who know nothing about finance and only knowledge of the economy is robux gift cards and use that as a point of reference"
+
+#Worked examples
+
+document_to_ppt(
+    doc = input_file_path, 
+    ppt = output_file_path_technical,
+    audience_type = audience_type1, 
+    slide_number = 3, 
+    file_format = "pptx"
+    )
+#document_to_ppt(
+#    doc = "inputs//Singapore Day 1.docx",
+#    ppt = "outputs//singapore.pptx", 
+#    audience_type = audience_type2, 
+#    slide_number = 3, 
+#    file_format = "docx"
+#    )
+
+#document_to_ppt(
+#    doc = "inputs//What is Sharpe Ratio.docx",
+#    ppt = "outputs//SSharpe.pptx",
+#    audience_type = audience_type2, 
+#    slide_number = 2, 
+#    file_format = "docx"
+#    )
+
+#document_to_ppt(
+#    doc = "inputs//What is Sharpe Ratio.docx",
+#    ppt = "outputs//SSharpe2.pptx",
+#    audience_type = audience_type3, 
+#    slide_number = 5, 
+#    file_format = "docx"
+#    )
+
+#document_to_ppt(
+#    doc = "inputs//abm_cc.pdf",
+#    ppt = "outputs//abm_robux.pptx",
+#    audience_type = audience_type3, 
+#    slide_number = 5, 
+#    file_format = "pdf"
+#    )
+#process_presentation(
+#    input_file_path, 
+#    output_file_path_technical, 
+#    audience_type1,
+#    executive_summary_slide = True)
+
+#process_presentation(
+#    input_file_path, 
+#    output_file_path_baby, 
+#    audience_type2)
